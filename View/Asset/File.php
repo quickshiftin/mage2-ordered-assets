5711534596d58213676bdb3afa63f3a79f8b2c7d
Nathan Nobbe <quickshiftin@gmail.com>
2018-08-16 14:05:43 -0600

    Use most specific interface possible.
    `Magento\Framework\View\Asset\File implements Magento\Framework\View\Asset\MergeableInterface` so we'll do the same in the plugin.

--- View/Asset/File.php
+++ View/Asset/File.php
@@ -1,16 +1,19 @@
 <?php
 namespace Quickshiftin\Assetorderer\View\Asset;
 
-use Magento\Framework\View\Asset\LocalInterface;
+use Magento\Framework\View\Asset\MergeableInterface;
 
-class File implements LocalInterface
+class File implements MergeableInterface
 {
     private $_iOrder = 1;
     private $_oRealFile;
 
+    // Implement AssetInterface
     public function getUrl() { return $this->_oRealFile->getUrl(); }
     public function getContentType() { return $this->_oRealFile->getContentType(); }
     public function getSourceContentType() { return $this->_oRealFile->getSourceContentType(); }
+
+    // Implement LocalInterface
     public function getSourceFile() { return $this->_oRealFile->getSourceFile(); }
     public function getContent() { return $this->_oRealFile->getContent(); }
     public function getFilePath() { return $this->_oRealFile->getFilePath(); }
@@ -18,6 +21,9 @@ class File implements LocalInterface
     public function getModule() { return $this->_oRealFile->getModule(); }
     public function getPath() { return $this->_oRealFile->getPath(); }
 
+    // @note MergeableInterface implements LocalInterface, but does not add any methods as of yet
+
+    // Forward everything else to the wrapped File instance
     public function __call($sMethod, array $aArgs=[])
     {
         return call_user_func_array([$this->_oRealFile, $sMethod], $aArgs);
