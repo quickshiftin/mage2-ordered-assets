89db673d237cad1d1e28ba2cd15309f2c30e6213
Nathan Nobbe <quickshiftin@gmail.com>
2018-08-16 13:55:09 -0600

    Update File.php

--- View/Asset/File.php
+++ View/Asset/File.php
@@ -1,10 +1,22 @@
 <?php
 namespace Quickshiftin\Assetorderer\View\Asset;
 
-class File
+use Magento\Framework\View\Asset\LocalInterface;
+
+class File implements LocalInterface
 {
     private $_iOrder = 1;
     private $_oRealFile;
+
+    public function getUrl() { return $this->_oRealFile->getUrl(); }
+    public function getContentType() { return $this->_oRealFile->getContentType(); }
+    public function getSourceContentType() { return $this->_oRealFile->getSourceContentType(); }
+    public function getSourceFile() { return $this->_oRealFile->getSourceFile(); }
+    public function getContent() { return $this->_oRealFile->getContent(); }
+    public function getFilePath() { return $this->_oRealFile->getFilePath(); }
+    public function getContext() { return $this->_oRealFile->getContext(); }
+    public function getModule() { return $this->_oRealFile->getModule(); }
+    public function getPath() { return $this->_oRealFile->getPath(); }
 
     public function __call($sMethod, array $aArgs=[])
     {
